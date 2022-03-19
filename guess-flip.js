32f152eb8f48aca57d45c8eb83c2ee2978555d1c
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 03:32:49 -0400

    Please just let this be it

--- guess-flip.js
+++ guess-flip.js
@@ -2,9 +2,9 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 
 var call = process.argv[2];
 
-if ((call == null && call != "heads")||(call == null && call != "tails")||( call != "heads" && call != "tails")) {
-    console.log("Error: no input. \nUsage: node guess-flip --call=[heads|tails]");
+if (call == "heads" || call == "tails") {
+    console.log(flipACoin(call));
 }
 else {
-    console.log(flipACoin(call));
+    console.log("Error: no input. \nUsage: node guess-flip --call=[heads|tails]");
 }
