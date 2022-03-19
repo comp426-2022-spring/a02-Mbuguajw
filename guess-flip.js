515087def5432336af68edff76610e2c914ce1ed
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 03:41:19 -0400

    im going to end it all, double dog dare me

--- guess-flip.js
+++ guess-flip.js
@@ -4,7 +4,6 @@ var call = process.argv[2];
 
 if (call == 'heads' || call == 'tails') {
     console.log(flipACoin(call));
-    process.exit(1);
 }
 else {
     console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
