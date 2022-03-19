7212d2aace40db23d8b8c17bfeb9fddba78f444c
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 12:29:11 -0400

    perhaps this may be the error

--- guess-flip.js
+++ guess-flip.js
@@ -5,6 +5,9 @@ var command = process.argv[2];
 if (command == 'heads' || command == 'tails') {
     console.log(flipACoin(call));
 }
-else {
+else if (command == null) {
     console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
+}
+else {
+    console.log("Error: wrong input.\nUsage: node guess-flip --call=[heads|tails]");
 }
