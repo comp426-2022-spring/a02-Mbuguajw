f338672cb279c2082bfac326fe79d789b4e0e4ae
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 03:19:33 -0400

    One bug fix, not all

--- guess-flip.js
+++ guess-flip.js
@@ -3,7 +3,7 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 var call = process.argv[2];
 
 if ((call == null && call != "heads")||(call == null && call != "tails")||( call != "heads" && call != "tails")) {
-    console.log("Error: no input.");
+    console.log("Error: no input. \nUsage: node guess-flip --call=[heads|tails]");
 }
 else {
     console.log(flipACoin(call));
