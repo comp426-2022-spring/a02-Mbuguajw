e244ffc1421551a42587aac1da04d51a98c8f8d7
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 03:34:06 -0400

    Its 3 am I am in too deep

--- guess-flip.js
+++ guess-flip.js
@@ -4,6 +4,7 @@ var call = process.argv[2];
 
 if (call == "heads" || call == "tails") {
     console.log(flipACoin(call));
+    process.exit(1);
 }
 else {
     console.log("Error: no input. \nUsage: node guess-flip --call=[heads|tails]");
