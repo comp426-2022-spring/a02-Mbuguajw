d82856249b55d12b1c38386b0df7a5378f435842
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 03:29:09 -0400

    go back GO BACK

--- guess-flip.js
+++ guess-flip.js
@@ -5,9 +5,6 @@ var call = process.argv[2];
 if ((call == null && call != "heads")||(call == null && call != "tails")||( call != "heads" && call != "tails")) {
     console.log("Error: no input. \nUsage: node guess-flip --call=[heads|tails]");
 }
-else if (call == "heads|tails") {
-    console.log(flipACoin("heads"));
-}
 else {
     console.log(flipACoin(call));
 }
