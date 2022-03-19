432a726021f7f6baca31c5a60fa6dced0ba7cd60
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 12:49:52 -0400

    Trial after trial again

--- guess-flip.js
+++ guess-flip.js
@@ -2,11 +2,11 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 
 var command = process.argv[2];
 
-if (command != 'heads' && command != 'tails') {
-    console.log("Error: wrong input.\nUsage: node guess-flip --call=[heads|tails]");
-}
-else if (command == null) {
+if (command == null) {
     console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
+}
+else if (command != 'heads' && command != 'tails') {
+    console.log("Error: wrong input.\nUsage: node guess-flip --call=[heads|tails]");
 }
 else {
     console.log(flipACoin(call));
