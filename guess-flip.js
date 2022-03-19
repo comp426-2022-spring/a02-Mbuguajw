620d383f2bfc181e2c0c17324dffacb4876b7449
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 03:39:29 -0400

    if the error is over a measely apostrophe i--

--- guess-flip.js
+++ guess-flip.js
@@ -2,10 +2,10 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 
 var call = process.argv[2];
 
-if (call == "heads" || call == "tails") {
+if (call == 'heads' || call == 'tails') {
     console.log(flipACoin(call));
     process.exit(1);
 }
 else {
-    console.log("Error: no input. \nUsage: node guess-flip --call=[heads|tails]");
+    console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
 }
