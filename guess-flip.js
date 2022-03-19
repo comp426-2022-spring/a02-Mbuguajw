56b67aaf3de67f7490cae8776aaa9712cc663947
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 12:41:23 -0400

    Reset

--- guess-flip.js
+++ guess-flip.js
@@ -2,12 +2,12 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 
 var command = process.argv[2];
 
-if (command != 'heads' && command == 'tails'||command == 'heads' && command != 'tails') {
-    console.log("Error: wrong input.\nUsage: node guess-flip --call=[heads|tails]");
+if (command == 'heads' || command == 'tails') {
+    console.log(flipACoin(call));
 }
 else if (command == null) {
     console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
 }
 else {
-    console.log(flipACoin(call));
+    console.log("Error: wrong input.\nUsage: node guess-flip --call=[heads|tails]");
 }
