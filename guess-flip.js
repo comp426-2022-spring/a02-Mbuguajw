9d04537d8023f05af7ee06f4b5fba09c2d7d4b2f
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 13:25:18 -0400

    okay but what about this

--- guess-flip.js
+++ guess-flip.js
@@ -4,7 +4,7 @@ var command = process.argv[2];
 if (command == null) {
     console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
 }
-else if ((command != 'heads') && (command != 'tails') && (command != 'heads'|'tails')) {
+else if ((command != 'heads') && (command != 'tails') && (command != 'heads|tails')) {
     console.log("Error: wrong input.\nUsage: node guess-flip --call=[heads|tails]");
 }
 else {
