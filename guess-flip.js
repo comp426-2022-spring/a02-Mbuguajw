37b8b5745fd436423e36a81c097c8fea7115fdad
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 13:24:02 -0400

    this doesnt even make sense anymore

--- guess-flip.js
+++ guess-flip.js
@@ -1,11 +1,10 @@
 import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 
 var command = process.argv[2];
-
 if (command == null) {
     console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
 }
-else if ((command != 'heads') && (command != 'tails')) {
+else if ((command != 'heads') && (command != 'tails') && (command != 'heads'|'tails')) {
     console.log("Error: wrong input.\nUsage: node guess-flip --call=[heads|tails]");
 }
 else {
