db473ef97fced02abfdc967d9a75183ffaa51e7b
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-19 12:47:45 -0400

    Gotta find the kink in this code

--- guess-flip.js
+++ guess-flip.js
@@ -2,12 +2,12 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 
 var command = process.argv[2];
 
-if (command == 'heads' || command == 'tails') {
-    console.log(flipACoin(call));
+if (command != 'heads' && command != 'tails') {
+    console.log("Error: wrong input.\nUsage: node guess-flip --call=[heads|tails]");
 }
 else if (command == null) {
     console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
 }
 else {
-    console.log("Error: wrong input.\nUsage: node guess-flip --call=[heads|tails]");
+    console.log(flipACoin(call));
 }
