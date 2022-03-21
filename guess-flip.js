e8a6eaa007a98793db460fad5c4c77dc7d1bbe87
Janet Mbugua <67240904+Mbuguajw@users.noreply.github.com>
2022-03-20 23:42:35 -0400

    Perhaps I imported it improperly

--- guess-flip.js
+++ guess-flip.js
@@ -1,6 +1,10 @@
 import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 
-var command = process.argv[2];
+import minimist from "minimist"
+const args = minimist(process.argv.slice(2))
+args['call']
+const command = args.call
+
 if (command === "heads" || command === "tails") {
     console.log(flipACoin(command));
 }
